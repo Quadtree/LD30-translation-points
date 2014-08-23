dd1e7c0d2980eb034fe468dcd82361841872e137
Eric Brown <quadtree@gmail.com>
2014-08-22 19:34:47 -0700

    Initial commit

---
+++ desktop/src/com/ironalloygames/ld30/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ironalloygames.ld30.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ironalloygames.ld30.LD30;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LD30(), config);
+	}
+}
